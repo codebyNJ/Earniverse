c2c0d9b982f7911df5eaf06d43df41dbb789d62e
codebyNJ <nijeesh10th@gmail.com>
2025-03-06 19:21:19 +0530

    second

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,34 @@
-import type React from "react"
-import "./globals.css"
-import type { Metadata } from "next"
-import { Poppins } from "next/font/google"
+import type React from "react";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
   weight: ["400", "500", "600", "700", "800"],
   subsets: ["latin"],
-})
+});
 
 export const metadata: Metadata = {
   title: "Earniverse - Motivate Students",
   description: "A space where every action leads to reward",
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
         <link
           href="https://fonts.googleapis.com/css2?family=Bungee+Shade&family=Cabin+Sketch:wght@400;700&family=Fredoka:wght@300..700&family=Pacifico&display=swap"
           rel="stylesheet"
@@ -30,6 +36,5 @@ export default function RootLayout({
       </head>
       <body className={poppins.className}>{children}</body>
     </html>
-  )
+  );
 }
-
